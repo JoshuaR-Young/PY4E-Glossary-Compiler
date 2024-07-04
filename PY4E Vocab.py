a5902128fbc3c1541f7de5d6112fdbf5a6e1fd49
Joshua Young <Joshuareiyoung@gmail.com>
2024-07-04 15:58:32 -0400

    PY4E Vocab.py

--- PY4E Vocab.py
+++ PY4E Vocab.py
@@ -1,9 +1,28 @@
 import sqlite3
 
-fhand = open('Py4e Vocab V2.txt')
+#read file and parse data
+filename = ('Py4e Vocab V2.txt')
+terms_definitions = []
 
-#process the file line for line
-for line in fhand:
-    words = line.split()
-    defin = words[1]
-    
+with open(filename, 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        if '|' in line:
+            term, definition = line.split('|', 1)
+            term = term.strip()
+            definition = definition.strip()
+            terms_definitions.append((term, definition))
+
+#connect to a SQL database or create one
+conn = sqlite3.connect('PY4Evocab.db')
+
+#create cursor object
+cursor = conn.cursor()
+
+#create a table
+create_table_query = '''
+CREATE TABLE IF NOT EXISTS glossary (
+    term TEXT PRIMARY KEY,
+    definition TEXT NOT NULL
+);
+'''
