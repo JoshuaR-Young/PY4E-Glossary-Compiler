e5e27b781088331b9e6e978471f75645c362d724
Joshua Young <Joshuareiyoung@gmail.com>
2024-07-06 19:46:33 -0400

    PY4E Vocab.py

--- PY4E Vocab.py
+++ PY4E Vocab.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-#read file and parse data
-filename = ('Py4e Vocab V2.txt')
+# Read file and parse data
+filename = 'Py4e Vocab V2.txt'
 terms_definitions = []
 
-with open(filename, 'r') as file:
+with open(filename, 'r', encoding='utf-8') as file:
     lines = file.readlines()
     for line in lines:
         if '|' in line:
@@ -13,13 +13,13 @@ with open(filename, 'r') as file:
             definition = definition.strip()
             terms_definitions.append((term, definition))
 
-#connect to a SQL database or create one
+# Connect to a SQL database or create one
 conn = sqlite3.connect('PY4Evocab.db')
 
-#create cursor object
+# Create cursor object
 cursor = conn.cursor()
 
-#create a table
+# Create a table
 create_table_query = '''
 CREATE TABLE IF NOT EXISTS glossary (
     term TEXT PRIMARY KEY,
@@ -28,13 +28,12 @@ CREATE TABLE IF NOT EXISTS glossary (
 '''
 cursor.execute(create_table_query)
 
-#insert data into table
+# Insert data into table
 insert_data_query = '''
-INSERT INTO glossary (term, definition) VALUES (?, ?)
-ON CONFLICT(term) DO NOTHING;
+INSERT OR IGNORE INTO glossary (term, definition) VALUES (?, ?);
 '''
 cursor.executemany(insert_data_query, terms_definitions)
 
-#commit changes and close
+# Commit changes and close
 conn.commit()
 conn.close()
